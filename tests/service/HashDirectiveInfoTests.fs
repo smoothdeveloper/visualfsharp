7301466bf1dfd0cbb1f73c7441d84cfca72e2783
gauthier <gauthier.segay@gmail.com>
2019-03-29 23:17:25 -0700

    cleanup the tests: data as test.
    They are all broken because the infrastructure those tests were initially using has changed; it seems to test against files under %tmp%.

--- tests/service/HashDirectiveInfoTests.fs
+++ tests/service/HashDirectiveInfoTests.fs
@@ -15,90 +15,72 @@ let dataFolderName = __SOURCE_DIRECTORY__ + "/data/"
 let getTestFilename filename =
     dataFolderName </> "ParsedLoadDirectives" </> filename
 
-let canonicalizeFilename filename = FileSystem.GetFullPathSafe filename
+let canonicalize filename = FileSystem.GetFullPathSafe filename
 
-let getAst filename = filename |> File.ReadAllText |> parseSource
+let getAst filename = 
+  filename 
+  |> getTestFilename
+  |> File.ReadAllText
+  |> parseSource
+
+let getDirectives filename =
+  filename 
+  |> getAst 
+  |> getIncludeAndLoadDirectives
+
+let getLoadDirectiveForExistingFileNamesOrNone filename =
+  filename
+  |> getDirectives
+  |> Array.choose (function | Load (loadDirective,range) -> Some (loadDirective,range) | _ -> None)
+  |> Array.map (function | (ExistingFile filename,range) -> range, Some (canonicalize filename) | (_, range) -> range, None)
+
+// checks #load resolves the same files, in same order, for the given cursor locations
+let checkRangesAndLoadFilesAreMatching filename expected =
+    let actuals = getLoadDirectiveForExistingFileNamesOrNone filename
+    Assert.AreEqual (expected, actuals)  
+  
+// checks #load resolves the same existing or unresolved files, in same order; it doesn't check the ranges
+let checkLoadFilesAreMatching filename expected =
+    let actuals = getLoadDirectiveForExistingFileNamesOrNone filename |> Array.map snd
+    let expected = expected |> Array.map (Option.map canonicalize)
+    Assert.AreEqual (expected, actuals)  
 
 [<Test>]
 let ``test1.fsx: verify parsed #load directives``() =
-   let ast = getAst (getTestFilename "test1.fsx")
-   let directives = getIncludeAndLoadDirectives ast
+    checkLoadFilesAreMatching 
+        "test1.fsx"
+        [| Some "includes/a.fs"
+           Some "includes/b.fs"
+           Some "includes/b.fs" |]
    
-   let expectedMatches =
-       [
-       Some (FileInfo(getTestFilename "includes/a.fs").FullName)
-       Some (FileInfo(getTestFilename "includes/b.fs").FullName)
-       Some (FileInfo(getTestFilename "includes/b.fs").FullName)
-       ]
-   
-   let results =
-       directives
-       |> Seq.map (function
-          | Load(ExistingFile(filename), _) -> Some ((new FileInfo(filename)).FullName)
-          | _ -> None
-       )
-       |> Seq.filter (Option.isSome)
-       |> Seq.toList
-   
-   Assert.AreEqual(expectedMatches, results)
 
 [<Test>]
 let ``test1.fsx: verify parsed position lookup of individual #load directives``() =
-    let ast = getAst (getTestFilename "test1.fsx")
-    
-    let expectations = [
-      (mkPos 1 1,       Some (FileInfo(getTestFilename "includes/a.fs").FullName))
-      (mkPos 1 5,       Some (FileInfo(getTestFilename "includes/a.fs").FullName))
-      (mkPos 2 1,       Some (FileInfo(getTestFilename "includes/b.fs").FullName))
-      (mkPos 2 5,       Some (FileInfo(getTestFilename "includes/b.fs").FullName))
-      (mkPos 3 1000,    None)
-      (mkPos 4 5,       Some (FileInfo(getTestFilename "includes/b.fs").FullName))
-    ]
-    
-    let results =
-        expectations
-        |> Seq.map fst
-        |> Seq.map (fun pos -> 
-           let result = getHashLoadDirectiveResolvedPathAtPosition pos ast
-           match result with
-           | None      -> pos, None
-           | Some path -> pos, Some (canonicalizeFilename path)
-        )
-        |> Seq.toList
-    
-    Assert.AreEqual(expectations, results)
+    checkRangesAndLoadFilesAreMatching
+        "test1.fsx"
+        [| mkPos 1 1  , Some "includes/a.fs"
+           mkPos 1 5  , Some "includes/a.fs"
+           mkPos 2 1  , Some "includes/b.fs"
+           mkPos 2 5  , Some "includes/b.fs"
+           mkPos 3 108, None
+           mkPos 4 5  , Some "includes/b.fs" |]
 
-[<Test(*;Ignore("this doesn't work for now")*)>]
-let ``single #load multiple files``() =
-    let ast = getAst (getTestFilename "single.load.multiple.files.fsx")
-
-    let expectations = [
-        (mkPos 1 1  , None)
-        (mkPos 1 108, None)
-        (mkPos 2 1  , None)
-        (mkPos 2 2  , None)
-        (mkPos 2 3  , Some (FileInfo(getTestFilename "test1.fsx")))
-        (mkPos 2 13 , Some (FileInfo(getTestFilename "test1.fsx")))
-        (mkPos 2 14 , Some (FileInfo(getTestFilename "test1.fsx")))
-        (mkPos 2 15 , Some (FileInfo(getTestFilename "includes/a.fs")))
-        (mkPos 2 29 , Some (FileInfo(getTestFilename "includes/a.fs")))
-        (mkPos 2 30 , Some (FileInfo(getTestFilename "includes/a.fs")))
-        (mkPos 2 31 , Some (FileInfo(getTestFilename "includes/b.fs")))
-        (mkPos 2 46 , Some (FileInfo(getTestFilename "includes/b.fs")))
-        (mkPos 2 108, None)
-        (mkPos 3 0  , None)
-        // todo: continue?
-    ]
-    
-    let results =
-        expectations
-        |> Seq.map fst
-        |> Seq.map (fun pos -> 
-           let result = getHashLoadDirectiveResolvedPathAtPosition pos ast
-           match result with
-           | None      -> pos, None
-           | Some path -> pos, Some (canonicalizeFilename path)
-        )
-        |> Seq.toList
-    
-    Assert.AreEqual(expectations, results) 
+[<Test;Ignore("this doesn't work for now")>]
+let ``single.load.multiple.files.fsx: can pinpoint exact file in a multiple files #load directive``() =
+    checkRangesAndLoadFilesAreMatching
+        "single.load.multiple.files.fsx"
+        [| mkPos 1 1  , None 
+           mkPos 1 108, None 
+           mkPos 2 1  , None 
+           mkPos 2 2  , None 
+           mkPos 2 3  , Some "test1.fsx"
+           mkPos 2 13 , Some "test1.fsx"
+           mkPos 2 14 , Some "test1.fsx"
+           mkPos 2 15 , Some "includes/a.fs"
+           mkPos 2 29 , Some "includes/a.fs"
+           mkPos 2 30 , Some "includes/a.fs"
+           mkPos 2 31 , Some "includes/b.fs"
+           mkPos 2 46 , Some "includes/b.fs"
+           mkPos 2 108, None 
+           mkPos 3 0  , None |]
+           // todo: continue?
