dafdf5c24b984ab46aa06147f3d2efce07f5044a
Gauthier Segay <gauthier.segay@gmail.com>
2020-01-22 08:25:44 +0100

    address code review nits / minimize diff / add comment on PrettifyDiscriminantAndTypePairs

--- src/fsharp/TypeChecker.fs
+++ src/fsharp/TypeChecker.fs
@@ -10379,8 +10379,8 @@ and TcMethodApplication
     
     // STEP 3. Resolve overloading 
     /// Select the called method that's the result of overload resolution
-    let finalCalledMeth =
-      
+    let finalCalledMeth = 
+
         let callerArgs = { Unnamed = unnamedCurriedCallerArgs ; Named = namedCurriedCallerArgs }
 
         let postArgumentTypeCheckingCalledMethGroup = 
